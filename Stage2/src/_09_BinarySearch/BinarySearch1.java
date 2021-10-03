b68ea60ef531e863d4745a8109196ba64468338d
LQF <liuqifeng19960918@outlook.com>
2021-10-03 11:22:54 +0800

    二分搜索树基本实现+添加元素

--- Stage2/src/_09_BinarySearch/BinarySearch1.java
+++ Stage2/src/_09_BinarySearch/BinarySearch1.java
@@ -5,44 +5,41 @@ public class BinarySearch1 {
     private BinarySearch1() {}
 
     public static <E extends Comparable<E>> int search(E[] data, E target) {
-        int l = 0, r = data.length - 1;
-
-        // 循环不变量：保持算法恒定在data[l, r]的范围中查找target；
-        // 所以关键是根据大小来更新l/r。
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (data[mid].compareTo(target) == 0) {
-                return mid;
-            } else if (data[mid].compareTo(target) < 0) {
-                l = mid + 1;
+        int L = 0, R = data.length - 1;
+        while (L <= R) {
+            int mid = L + (R - L) / 2;
+            if (data[mid].compareTo(target) < 0) {
+                L = mid + 1;
+            } else if (data[mid].compareTo(target) > 0) {
+                R = mid - 1;
             } else {
-                r = mid - 1;
+                return mid;
             }
         }
         return -1;
     }
 
     /**
-     * 查找大于target的最小值的索引
+     * upper（上界）：查找大于target  的最小值的索引
      */
     public static <E extends Comparable<E>> int upper(E[] data, E target) {
-        int l = 0, r = data.length;
-
-        // 在data[l, r]中寻找解
-        while (l < r) {
-            int mid = l + (r - l) / 2;
+        int L = 0, R = data.length;
+        while (L < R) {
+            int mid = L + (R - L) / 2;
             if (data[mid].compareTo(target) <= 0) {
-                l = mid + 1;
+                L = mid + 1;
             } else {
-                r = mid;
+                R = mid;
             }
         }
-        return l;
+        return L;
     }
 
+
     /**
-     * >target，返回最小值索引I
-     * == target，返回最大索引
+     * ceil（天花板）：
+     * 若存在等于target，返回target值的最大索引
+     * 若不存在target，返回大于target的最小值索引
      * @param data
      * @param target
      * @param <E>
@@ -56,21 +53,26 @@ public class BinarySearch1 {
         return u;
     }
 
-    // <target 的最大值索引
-    public static <E extends Comparable<E>> int lower(E[] data, E target) {
-        int l = -1, r = data.length - 1;
+    // 返回target 的最大值索引
 
-        // 在data[l, r]中寻找解
-        while (l < r) {
-            // 下取整，若不加1，则会导致bug；
-            // 所以加1，调整为上取整。
-            int mid = l + (r - l + 1) / 2;
-            if (data[mid].compareTo(target) < 0) {
-                l = mid;
+    /**
+     * lower：下界；
+     * 查找小于target的元素中最大的那一个。
+     * @param data
+     * @param target
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable<E>> int lower(E[] data, E target) {
+        int L = -1, R = data.length - 1;
+        while (L < R) {
+            int mid = L + (R - L + 1) / 2;
+            if (data[mid].compareTo(target) >= 0) {
+                R = mid - 1;
             } else {
-                r = mid - 1;
+                L = mid;
             }
         }
-        return l;
+        return L;
     }
 }
